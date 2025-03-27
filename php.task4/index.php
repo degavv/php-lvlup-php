105f65a1f895a45392b4805f9ebd15a8bd856d6d
dega <devv@mail.com>
2025-03-27 19:38:52 +0200

    task4 Complete validation input values

--- php.task4/index.php
+++ php.task4/index.php
@@ -2,32 +2,65 @@
 
 $_width = $_GET["width"] ?? null;
 $_length = $_GET["length"] ?? null;
-$width = (float) $_width;
+
 $length = (float) $_length;
 const PLINTH_LEN = 2;
 $width_valid_msg = null;
 $length_valid_msg = null;
 
-// Валідація вхідних даних
 
-if ($width == null) {
+// Валідація вхідних даних
+// Перевірка довжини
+if ($_width === null) {
     $width_valid_msg = "Значення не введено";
-} elseif ($width == ""){
+} elseif ($_width === "") {
     $width_valid_msg = "Значення не може бути порожнє";
-} elseif ($width < 0 ){
-    $width_valid_msg = "Значення не може бути від'ємне";
-} elseif ($width == 0 ){
-    $width_valid_msg = "Значення не може бути нульовим";
+} else {
+    $width = (float) $_width;
+    if ($width === 0.0 && $_width !== '0') {
+        $width_valid_msg = "Значення не має складатися з літер";
+    } elseif ($width < 0) {
+        $width_valid_msg = "Значення не може бути від'ємне";
+    } elseif ($width === 0.0) {
+        $width_valid_msg = "Значення не може бути нульовим";
+    } elseif ($width > 100) {
+        $width_valid_msg = "Значення не може бути більше 100";
+    }
+}
+// Перевірка ширини
+if ($_length === null) {
+    $length_valid_msg = "Значення не введено";
+} elseif ($_length === "") {
+    $length_valid_msg = "Значення не може бути порожнє";
+} else {
+    $length = (float) $_length;
+    if ($length === 0.0 && $_length !== '0') {
+        $length_valid_msg = "Значення не має складатися з літер";
+    } elseif ($length < 0) {
+        $length_valid_msg = "Значення не може бути від'ємне";
+    } elseif ($length === 0.0) {
+        $length_valid_msg = "Значення не може бути нульовим";
+    } elseif ($length > 100) {
+        $length_valid_msg = "Значення не може бути більше 100";
+    }
+}
+
+if (!($length_valid_msg && $width_valid_msg)) {
+    $area = $width * $length;
+    if ($area < 2) {
+        $plinth_quantity = 1;
+    } else {
+        $plinth_quantity = $area / PLINTH_LEN;
+    }
 }
 
 
 
 
-$plinth_quantity = ($width * $length) / PLINTH_LEN;
 $foo = ($width * $length) % PLINTH_LEN;
 $bar = (int) $plinth_quantity;
 // var_dump( $width, $length );
-var_dump($plinth_quantity, $foo, $bar);
+// var_dump($plinth_quantity, $foo, $bar);
 
 ?>
 <!DOCTYPE html>
@@ -44,18 +77,28 @@ var_dump($plinth_quantity, $foo, $bar);
     <h1>Розрахунок кількості необхідних плінтусів</h1>
     <form action="/index.php">
         <label for="length">Довжина кімнати</label>
-        <input type="number" name="length" value="<?=$_length ?>" id="length" step="0.1" placeholder="Введіть довжину кімнати">
-        <?php if ($length_valid_msg):?>
-            <p><?=$length_valid_msg ?></p>
-        <?php endif ?>
+        <input type="number" name="length" value="<?= $_length ?>" id="length" step="0.1"
+            placeholder="Введіть довжину кімнати">
+        <div>
+            <?php if ($length_valid_msg): ?>
+                <p><?= $length_valid_msg ?></p>
+            <?php endif ?>
+        </div>
         <label for="width">Ширина кімнати</label>
-        <input type="number" name="width" value="<?=$_width ?>" id="width" step="0.1" placeholder="Введіть ширину кімнати">
-        <?php if ($width_valid_msg):?>
-            <p><?=$width_valid_msg ?></p>
-        <?php endif ?>
+        <input type="number" name="width" value="<?= $_width ?>" id="width" step="0.1"
+            placeholder="Введіть ширину кімнати">
+        <div>
+            <?php if ($width_valid_msg): ?>
+                <p><?= $width_valid_msg ?></p>
+            <?php endif ?>
+        </div>
         <button type="submit">Розрахувати</button>
     </form>
-    <div><?= $plinth_quantity ?></div>
+    <?php if ($length_valid_msg && $width_valid_msg): ?>
+        <div>Загальна площа кімнати складає = <?= $area ?> м<sup>2</sup></div><br> <!--TODO delete br -->
+        <div>Необхідна кількості плінтусів = <?= $plinth_quantity ?></div>
+    <?php endif ?>
+
 </body>
 
 </html>
