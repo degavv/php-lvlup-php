cf068de6f4f179e397f7fed25c906bb76b493b9c
dega <devv@mail.com>
2025-03-31 21:08:34 +0300

    task5 few fixes

--- php.task5/sum/index.php
+++ php.task5/sum/index.php
@@ -8,42 +8,43 @@ $errors_list = [
     "float" => "Число не може бути дробовим",
     "less" => "Число має бути більшим за початкове",
 ];
-$validation_errors = [];
+
+$error_msg = [];
 $is_valid = false;
 $sum = 0;
 $interval = "";
 
-if ($input_start == null) {
-    $validation_errors["start"] = $errors_list["null"];
-    echo "NULL";
+
+var_dump($input_start === null);
+if ($input_start === null) {
+    $error_msg["start"] = $errors_list["null"];
 } elseif ($input_start === "") {
-    $validation_errors["start"] = $errors_list["empty"];
+    $error_msg["start"] = $errors_list["empty"];
 } else {
     $num_start = (int) $input_start;
     if ($num_start === 0 && $input_start !== "0") {
-        $validation_errors["start"] = $errors_list["chars"];
+        $error_msg["start"] = $errors_list["chars"];
     } elseif (($num_start - $input_start) !== 0) {
-        $validation_errors["start"] = $errors_list["float"];
+        $error_msg["start"] = $errors_list["float"];
     }
 }
 
 if ($input_end == null) {
-    $validation_errors["end"] = $errors_list["null"];
-    echo "NULL";
+    $error_msg["end"] = $errors_list["null"];
 } elseif ($input_end === "") {
-    $validation_errors["end"] = $errors_list["empty"];
+    $error_msg["end"] = $errors_list["empty"];
 } else {
     $num_end = (int) $input_end;
     if ($num_end === 0 && $input_end !== "0") {
-        $validation_errors["end"] = $errors_list["chars"];
+        $error_msg["end"] = $errors_list["chars"];
     } elseif (($num_end - $input_end) !== 0) {
-        $validation_errors["end"] = $errors_list["float"];
+        $error_msg["end"] = $errors_list["float"];
     } elseif ($num_end <= $num_start) {
-        $validation_errors["end"] = $errors_list["less"];
+        $error_msg["end"] = $errors_list["less"];
     }
 }
 
-if (count($validation_errors) == 0) {
+if (count($error_msg) == 0) {
     $is_valid = true;
 }
 
@@ -76,17 +77,17 @@ if ($is_valid) {
         <h1>Підрахунок суми чисел</h1>
         <form action="/sum/index.php" method="post">
             <label for="start">Початкове значення</label>
-            <input type="number" name="start" id="start" placeholder="Введіть початкове значення" value="<?= $validation_errors["end"] ?? '' ? $input_start : '' ?>">
+            <input type="number" name="start" id="start" placeholder="Введіть початкове значення" value="<?= $error_msg["end"] ?? '' ? $input_start : '' ?>">
             <div>
-                <?php if ($validation_errors["start"] ?? null): ?>
-                    <p><?= $validation_errors["start"] ?></p>
+                <?php if ($error_msg["start"] ?? null): ?>
+                    <p><?= $error_msg["start"] ?></p>
                 <?php endif ?>
             </div>
             <label for="end">Кінцеве значення</label>
-            <input type="number" name="end" id="end" placeholder="Введіть кінцеве значення" value="<?= $validation_errors["start"] ?? '' ? $input_end : '' ?>">
+            <input type="number" name="end" id="end" placeholder="Введіть кінцеве значення" value="<?= $error_msg["start"] ?? '' ? $input_end : '' ?>">
             <div>
-                <?php if ($validation_errors["end"] ?? null): ?>
-                    <p><?= $validation_errors["end"] ?></p>
+                <?php if ($error_msg["end"] ?? null): ?>
+                    <p><?= $error_msg["end"] ?></p>
                 <?php endif ?>
             </div>
             <button type="submit">Розрахувати</button>
