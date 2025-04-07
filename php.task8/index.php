2202d9b242543d9e0e537e7ffaaaf541796a6c82
dega <mail@gmail.com>
2025-04-07 16:54:43 +0300

    php.task8 is completed

--- php.task8/index.php
+++ php.task8/index.php
@@ -1,22 +1,80 @@
 <?php
-function validateInputValues(): bool|array
+function validateInputValues(): array
 {
-    $num = $_GET["number"];
-    $deg = $_GET["degree"];
-    return ["number" => $num, "degree" => $deg];
+    $errors = [];
+    $num = $_GET["number"] ?? null;
+    $pow = $_GET["power"] ?? null;
+
+    if ($num === null || $num === '') {
+        $errors[] = "Число не введено";
+    } elseif (!is_numeric($num)) {
+        $errors[] = "Число має бути числовим значенням";
+    } else {
+        $number = (float) $num;
+    }
+
+    if ($pow === null || $pow === '') {
+        $errors[] = "Степінь не введено";
+    } elseif (!is_numeric($pow)) {
+        $errors[] = "Показник степеня має бути числовим значенням.";
+    } else {
+        $power = (int) $pow;
+    }
+
+    if (empty($errors)) {
+        return [
+            'valid' => true,
+            'number' => (float) $number,
+            'power' => (int) $power,
+        ];
+    }
+
+    return [
+        'valid' => false,
+        'errors' => $errors,
+    ];
 }
 
-function exponent($numAndDegree){
-    $num = $numAndDegree["number"];
-    $deg = $numAndDegree["degree"];
-    $result = $num;
-    for ($i = 1; $i < $deg; $i++) {
-        $result *= $num;
+function exponent($number, $power): int|float
+{
+    $isNegativePow = false;
+    if ($power == 0) {
+        return 1;
+    } elseif ($power < 0) {
+        $power *= -1;
+        $isNegativePow = true;
+    }
+
+    $result = $number;
+    for ($i = 1; $i < $power; $i++) {
+        $result *= $number;
+    }
+
+    if ($isNegativePow) {
+        return 1 / $result;
     }
     return $result;
 }
-$res = exponent(validateInputValues());
-var_dump($res);
+
+function main() {
+    $inputValues = validateInputValues();
+
+    if ($inputValues['valid']) {
+        $number = $inputValues['number'];
+        $power =  $inputValues['power'];
+        $res = exponent($number, $power);
+        return $res;
+    } else {
+        $errorMsg = '';
+        foreach ($inputValues["errors"] as $error) {
+            $errorMsg .= "<br>" . $error . "<br>";
+        }
+        return $errorMsg;
+    }
+}
+
+$res = main();
+
 ?>
 <!DOCTYPE html>
 <html lang="uk">
@@ -33,10 +91,14 @@ var_dump($res);
     <form action="/index.php">
         <label for="number">Число</label>
         <input type="text" id="number" name="number" placeholder="Введіть число">
-        <label for="degree">Степінь</label>
-        <input type="text" id="degree" name="degree" placeholder="Введіть степінь">
+        <label for="power">Степінь</label>
+        <input type="text" id="power" name="power" placeholder="Введіть степінь">
         <button type="submit">Розрахувати результат</button>
     </form>
+    <div>
+        <span class="result">Результат: </span>
+        <span class="error"><?= $res ?></span>
+    </div>
 </body>
 
 </html>
