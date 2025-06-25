4fe0b8b6e451f4a86822ac3bada13b6bf2d8ff4c
dega <mail@gmail.com>
2025-06-25 16:57:42 +0300

    project2306_js is completed

--- project2306_js/person.js
+++ project2306_js/person.js
@@ -11,60 +11,93 @@ function Person(name, secondName, year, month, day, city) {
 function PersonFarm() {
     this.persons = [];
     this.names = ['Nick', 'Bob', 'Tim', 'Bread', 'Rita', 'Max', 'Tom', 'Den', 'Emma', 'Steven', 'Zack', 'Bill',];
-    this.secondNames = ['Prokopenko', 'Opanasenko', 'Shevchenko', 'Lysenko', 'Nikolenko', 'Zinchenko', 'Denysenko', 'Tymoshenko', 'Amosova', 'Pupko', 'Shmatko', 'Dmytrenko',];
+    this.secondNames = ['Prokopenko', 'Opanasenko', 'Shevchenko', 'Lysenko', 'Nikolenko', 'Zinchenko', 'Denysenko', 'Tymoshenko', 'Amosenko', 'Pupko', 'Shmatko', 'Dmytrenko',];
+    this.cities = ['Lviv', 'Kyiv', 'Odesa', 'Kharkiv', 'Dnipro', 'Kherson', 'Lutsk', 'Yalta', 'Rivne',];
+
+    this.create = function (count) {
+        for (i = 0; i <= count; i++) {
+            this.add(
+                this.getRandomItem(this.names),
+                this.getRandomItem(this.secondNames),
+                this.getRandomInt(1990, 2000),
+                this.getRandomInt(0, 11),
+                this.getRandomInt(1, 31),
+                this.getRandomItem(this.cities),
+            )
+        }
+    }
 
     this.getRandomInt = function (min, max) {
         let rand = Math.floor(Math.random() * (max - min + 1) + min);
         return rand;
     }
+
+    this.getRandomItem = function (array) {
+        let randIndex = this.getRandomInt(0, array.length - 1);
+        return array[randIndex];
+    }
+
     this.add = function (name, secondName, year, month, day, city) {
         let person = new Person(name, secondName, year, month, day, city);
         return this.persons.push(person);
     }
+
     this.sort = function () {
+        let self = this;
         this.persons.sort(function (a, b) {
-            if (a.city > b.city) {
-                return 1;
-            } else if (a.city < b.city) {
-                return -1;
-            } else if (a.city === b.city) {
-                if (a.birthDate.getTime() > b.birthDate.getTime()) {
-                    return -1;
-                } else if (a.birthDate.getTime() < b.birthDate.getTime()) {
-                    return 1;
-                } else if (a.birthDate.getTime() === b.birthDate.getTime()) {
-                    if (a.secondName > b.secondName) {
-                        return 1;
-                    } else if (a.secondName === b.secondName) {
-                        return 0;
-                    } else if (a.secondName < b.secondName) {
-                        return -1;
-                    }
-                }
+            let res = self.sortByCity(a, b);
+            if (res !== 0) {
+                return res;
             }
-        })
+
+            res = self.sortByAge(a, b);
+            if (res !== 0) {
+                return res;
+            }
+
+            return self.sortBySecondName(a, b);
+        });
+    }
+
+    this.sortByCity = function (a, b) {
+        if (a.city > b.city) {
+            return 1;
+        } else if (a.city < b.city) {
+            return -1;
+        } else if (a.city === b.city) {
+            return 0;
+        }
+    }
+
+    this.sortByAge = function (a, b) {
+        if (a.birthDate.getTime() > b.birthDate.getTime()) {
+            return -1;
+        } else if (a.birthDate.getTime() < b.birthDate.getTime()) {
+            return 1;
+        } else if (a.birthDate.getTime() === b.birthDate.getTime()) {
+            return 0;
+        }
+    }
+
+    this.sortBySecondName = function (a, b) {
+        if (a.secondName > b.secondName) {
+            return 1;
+        } else if (a.secondName === b.secondName) {
+            return 0;
+        } else if (a.secondName < b.secondName) {
+            return -1;
+        }
     }
 }
 
-let farm = new PersonFarm();
-farm.add('Nick', 'Prokopenko', 1999, 10, 23, 'Lviv');
-farm.add('Bob', 'Opanasenko', 1986, 4, 13, 'Kyiv');
-farm.add('Tim', 'Shevchenko', 1997, 8, 8, 'Odesa');
-farm.add('Bread', 'Lysenko', 2000, 5, 17, 'Kharkiv');
-farm.add('Rita', 'Nikolenko', 2002, 11, 10, 'Lviv');
-farm.add('Max', 'Zinchenko', 1995, 4, 23, 'Dnipro');
-farm.add('Tom', 'Denysenko', 1974, 6, 3, 'Kherson');
-farm.add('Den', 'Tymoshenko', 1988, 0, 11, 'Lutsk');
-farm.add('Emma', 'Amosova', 1999, 10, 23, 'Lviv');
-farm.add('Steven', 'Pupko', 1969, 11, 20, 'Rivne');
-farm.add('Zack', 'Shmatko', 1990, 2, 14, 'Lviv');
-farm.add('Bill', 'Dmytrenko', 1992, 3, 4, 'Yalta');
-farm.sort();
-
-// for (let i = 0; i < farm.persons.length; i++) {
-//     alert(farm.persons[i]);
-// }
-
-for (let i = 0; i < 50; i++) {
-    console.log(farm.getRandomInt(0, 9));
+function init() {
+    let farm = new PersonFarm();
+    farm.create(11);
+    farm.sort();
+    console.log(farm.persons);
+    for (let i = 0; i < farm.persons.length; i++) {
+        alert(farm.persons[i]);
+    }
 }
+
+init();
--- project2306_js/person_default.js
+++ project2306_js/person_default.js
@@ -9,51 +9,80 @@ function Person(name, secondName, year, month, day, city) {
 }
 
 function PersonFarm() {
-    this.persons = [];  
+    this.persons = [];
     this.add = function (name, secondName, year, month, day, city) {
         let person = new Person(name, secondName, year, month, day, city);
         return this.persons.push(person);
     }
+
     this.sort = function () {
+        let self = this;
         this.persons.sort(function (a, b) {
-            if (a.city > b.city) {
-                return 1;
-            } else if (a.city < b.city) {
-                return -1;
-            } else if (a.city === b.city) {
-                if (a.birthDate.getTime() > b.birthDate.getTime()) {
-                    return -1;
-                } else if (a.birthDate.getTime() < b.birthDate.getTime()) {
-                    return 1;
-                } else if (a.birthDate.getTime() === b.birthDate.getTime()) {
-                    if (a.secondName > b.secondName) {
-                        return 1;
-                    } else if (a.secondName === b.secondName) {
-                        return 0;
-                    } else if (a.secondName < b.secondName) {
-                        return -1;
-                    }
-                }
+            let res = self.sortByCity(a, b);
+            if (res !== 0) {
+                return res;
             }
-        })
+
+            res = self.sortByAge(a, b);
+            if (res !== 0) {
+                return res;
+            }
+
+            return self.sortBySecondName(a, b);
+        });
+    }
+
+    this.sortByCity = function (a, b) {
+        if (a.city > b.city) {
+            return 1;
+        } else if (a.city < b.city) {
+            return -1;
+        } else if (a.city === b.city) {
+            return 0;
+        }
+    }
+
+    this.sortByAge = function (a, b) {
+        if (a.birthDate.getTime() > b.birthDate.getTime()) {
+            return -1;
+        } else if (a.birthDate.getTime() < b.birthDate.getTime()) {
+            return 1;
+        } else if (a.birthDate.getTime() === b.birthDate.getTime()) {
+            return 0;
+        }
+    }
+
+    this.sortBySecondName = function (a, b) {
+        if (a.secondName > b.secondName) {
+            return 1;
+        } else if (a.secondName === b.secondName) {
+            return 0;
+        } else if (a.secondName < b.secondName) {
+            return -1;
+        }
     }
 }
 
-let farm = new PersonFarm();
-farm.add('Nick', 'Prokopenko', 1999, 10, 23, 'Lviv');
-farm.add('Bob', 'Opanasenko', 1986, 4, 13, 'Kyiv');
-farm.add('Tim', 'Shevchenko', 1997, 8, 8, 'Odesa');
-farm.add('Bread', 'Lysenko', 2000, 5, 17, 'Kharkiv');
-farm.add('Rita', 'Nikolenko', 2002, 11, 10, 'Lviv');
-farm.add('Max', 'Zinchenko', 1995, 4, 23, 'Dnipro');
-farm.add('Tom', 'Denysenko', 1974, 6, 3, 'Kherson');
-farm.add('Den', 'Tymoshenko', 1988, 0, 11, 'Lutsk');
-farm.add('Emma', 'Amosova', 1999, 10, 23, 'Lviv');
-farm.add('Steven', 'Pupko', 1969, 11, 20, 'Rivne');
-farm.add('Zack', 'Shmatko', 1990, 2, 14, 'Lviv');
-farm.add('Bill', 'Dmytrenko', 1992, 3, 4, 'Yalta');
-farm.sort();
+function init() {
+    let farm = new PersonFarm();
+    farm.add('Nick', 'Prokopenko', 1999, 10, 23, 'Lviv');
+    farm.add('Bob', 'Opanasenko', 1986, 4, 13, 'Kyiv');
+    farm.add('Tim', 'Shevchenko', 1997, 8, 8, 'Odesa');
+    farm.add('Bread', 'Lysenko', 2000, 5, 17, 'Kharkiv');
+    farm.add('Rita', 'Nikolenko', 2002, 11, 10, 'Lviv');
+    farm.add('Max', 'Zinchenko', 1995, 4, 23, 'Dnipro');
+    farm.add('Tom', 'Denysenko', 1974, 6, 3, 'Kherson');
+    farm.add('Den', 'Tymoshenko', 1988, 0, 11, 'Lutsk');
+    farm.add('Emma', 'Amosova', 1999, 10, 23, 'Lviv');
+    farm.add('Steven', 'Pupko', 1969, 11, 20, 'Rivne');
+    farm.add('Zack', 'Shmatko', 1990, 2, 14, 'Lviv');
+    farm.add('Bill', 'Dmytrenko', 1992, 3, 4, 'Yalta');
+    farm.sort();
+    console.log(farm.persons);
 
-for (let i = 0; i < farm.persons.length; i++) {
-    alert(farm.persons[i]);
+    for (let i = 0; i < farm.persons.length; i++) {
+        alert(farm.persons[i]);
+    }
 }
+
+init();
