352693f90978c1b241f869f685f8dc2e894ecdab
dega <mail@gmail.com>
2025-07-09 15:24:07 +0300

    project0207 PhotoValidator added

---
+++ project0207_js/app/validators/PhotoValidator.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace app\validators;
+
+class PhotoValidator
+{
+    public static function validate($uploadedFile): array
+    {
+        $errors = [];
+
+        if (!isset($uploadedFile) || $uploadedFile['error'] !== UPLOAD_ERR_OK) {
+            $errors[] = FILE_UPLOAD_ERRORS[$uploadedFile['error']] ?? 'Невідома помилка завантаження.';
+            return $errors;
+        }
+
+        // MIME-тип
+        if (!in_array($uploadedFile['type'], PHOTO_AVAILABLE_TYPES, true)) {
+            $errors[] = FILE_UPLOAD_ERRORS[5]; // Недопустимий тип файла
+        }
+
+        // Розмір
+        if ($uploadedFile['size'] > PHOTO_MAX_FILE_SIZE) {
+            $errors[] = FILE_UPLOAD_ERRORS[2]; // Завеликий файл
+        }
+
+        return $errors;
+    }
+}
